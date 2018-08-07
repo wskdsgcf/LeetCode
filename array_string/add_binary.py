7dcb00a70273c753603f648f4587d2c588416315
wskdsgcf <wskdsgcf@hotmail.com>
2018-08-07 16:26:30 +0800

    add binary

---
+++ array_string/add_binary.py
@@ -0,0 +1,12 @@
+def addBinary(a, b):
+    """
+    :type a: str
+    :type b: str
+    :rtype: str
+    """
+    return bin(int(a, 2) + int(b, 2))[2:]
+
+
+if __name__ == '__main__':
+    print(addBinary('11', '1'))
+    
---
+++ array_string/add_binary_good.py
@@ -0,0 +1,7 @@
+def addBinary(self, a, b):
+    """
+    :type a: str
+    :type b: str
+    :rtype: str
+    """
+    return format(int(a,2)+int(b,2),"b")
